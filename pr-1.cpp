3e8d0308942de61c80675694dc5417f018919e83
pri-245 <geetaghinaiya01@gmail.com>
2025-05-09 10:53:48 +0530

    Add files via upload

---
+++ pr-1.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+{
+	float F,C;
+	
+	printf("Enter C:");
+	scanf("%f",&C);
+	
+	
+	F=(1.8*C)+32;
+	
+	printf("F is:%f",F);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
